a963ab30a442581160f40ca506449c9a9c2738ff
Sam Kim <samkim90274@gmail.com>
2020-04-22 23:30:24 -0400

    Move pretty_date into own module

--- app.py
+++ app.py
@@ -1,69 +1,18 @@
 from collections import namedtuple
 
-from pytz import timezone
-
 import boto3
-import pytz
 from flask import Flask, render_template
 
-Cropped = namedtuple("Cropped", "name url last_updated timestamp")
+from pretty_date import pretty_date
+
+Screenshot = namedtuple("Screenshot", "name url last_updated timestamp")
 
 BUCKET_NAME = "pokerstars-90274"
 
 app = Flask(__name__)
 
 
-def pretty_date(time=False):
-    """
-    Get a datetime object or a int() Epoch timestamp and return a
-    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
-    'just now', etc
-
-    Adapted from: https://stackoverflow.com/a/1551394
-    """
-    from datetime import datetime
-    now = datetime.now(pytz.utc)
-    if type(time) is int:
-        diff = now - datetime.fromtimestamp(time)
-    elif isinstance(time,datetime):
-        diff = now - time
-    elif not time:
-        diff = now - now
-    second_diff = diff.seconds
-    day_diff = diff.days
-
-    if day_diff < 0:
-        return ''
-
-    if day_diff == 0:
-        if second_diff < 10:
-            return "just now"
-        if second_diff < 60:
-            return str(second_diff) + " seconds ago"
-        if second_diff < 120:
-            return "a minute ago"
-        if second_diff < 3600:
-            return str(second_diff // 60) + " minutes ago"
-        if second_diff < 7200:
-            return "an hour ago"
-        if second_diff < 86400:
-            return str(second_diff // 3600) + " hours ago"
-    if day_diff == 1:
-        return "Yesterday"
-    if day_diff < 7:
-        return str(day_diff) + " days ago"
-    if day_diff < 14:
-        return str(day_diff // 7) + " week ago"
-    if day_diff < 31:
-        return str(day_diff // 7) + " weeks ago"
-    if day_diff < 365:
-        return str(day_diff // 30) + " months ago"
-    if day_diff < 365*2:
-        return str(day_diff // 365) + " year ago"
-    return str(day_diff // 365) + " years ago"
-
-
-def _get_cropped():
+def pull_screenshots(key_prefix):
     s3 = boto3.client("s3")
     response = s3.list_objects(Bucket=BUCKET_NAME)
     keys = [
